7469a1bc3ea85f24fa777facac37bb17b13ba893
therealbeez <gabriel.agbese2@gmail.com>
2022-12-02 14:23:08 +0100

    installed and implement typewriter

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,18 +5,19 @@ import memoji from "../images/mem.png"
 import { SlSocialLinkedin } from "react-icons/sl"
 import { CgFileDocument } from "react-icons/cg"
 import { useState } from "react";
+import Typewriter from 'typewriter-effect';
 import TextAnimation from "react-text-animations";
 
-const Hero = ({fn}) => {
+const Hero = ({ fn }) => {
 
     useEffect(() => {
-        
+
     },);
-    
-    return(
+
+    return (
         <div className="wrapper2 text">
             <div className="content2">
-               <div className="hero">
+                <div className="hero">
                     <div className="hero-underlier">
                         <div className="hero-light"></div>
                         <div className="hero-stars">
@@ -26,19 +27,28 @@ const Hero = ({fn}) => {
                     </div>
                     <div className="herowrapper ">
                         <div className="top-space"></div>
-                        <div className="hey-there">Hey there, I'm-</div>
+                        <div className="hey-there">Hello there, I'm-</div>
                         <div className="first-name">Gabriel</div>
                         <div className="description">Frontend Developer,<span className="darker"> specializing in custom layouts</span></div>
-                        <div className="opportunity">Open to Internship opportunities</div>
-                        <div className="hero-links">
-                            <div className="link blur">Github</div>
-                            <div className="link blur">LinkedIn</div>
-                            <div className="link blur">Email</div>
+                        <div className="opportunity">
+                            <p>Open to&ensp; </p>
+                            <Typewriter
+                                options={{
+                                    strings: ['Internship', 'Part-Time','Freelance'],
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            />
+                            <p>&ensp;opportunities</p>
                         </div>
+                        <div className="hero-links">
+                                <div className="link blur">Github</div>
+                                <div className="link blur">LinkedIn</div>
+                                <div className="link blur">Email</div>
+                            </div>
                         <div className="planet"></div>
                     </div>
-                    
-               </div>
+                </div>
             </div>
         </div>
     )
