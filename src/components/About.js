aac42d01525ae1f6a9a0c635d84c2eaa7db3a7c7
therealbeez <gabriel.agbese2@gmail.com>
2022-12-20 19:25:52 +0100

    skills added

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ import { CgFileDocument } from "react-icons/cg"
 
 
 
-const About = ({fn}) => {
+const About = ({ fn }) => {
 
     useEffect(() => {
 
@@ -19,13 +19,13 @@ const About = ({fn}) => {
         var first = document.getElementById("first")
         var second = document.getElementById("second")
 
-        if(check){
+        if (check) {
             first.classList.add("activeTab");
             biotab.classList.add("highlightTab");
         }
 
 
-        biotabradio.addEventListener("click", function(){
+        biotabradio.addEventListener("click", function () {
             skillstabradio.checked = false;
             check = this.checked
             second.classList.remove("activeTab")
@@ -34,8 +34,8 @@ const About = ({fn}) => {
             biotab.classList.add("highlightTab")
         });
 
-        skillstabradio.addEventListener("click", function(){
-            biotabradio.checked = false;    
+        skillstabradio.addEventListener("click", function () {
+            biotabradio.checked = false;
             check = this.checked
             first.classList.remove("activeTab")
             biotab.classList.remove("highlightTab")
@@ -44,47 +44,105 @@ const About = ({fn}) => {
         });
 
     },);
-  
-    return(
+
+    return (
         <div className="wrapper2 text">
             <div className="content2">
-            <div className="about section abouttext">
-                <div className="side">
-                <div className="">
-                    <p className="bigtext header">About</p>
-                    <div className="classname"></div>
-                </div>
-                <div className="linksbio">
-                    <div className="links midtext"><img src={memoji} alt="BigCo Inc. logo" className="memoji"/></div>
-                    <div className="biotext smalltext">
-                        <div className="about-button blur midtext">
-                            <p className="smidtext"><a className="default-link" href="https://www.linkedin.com/in/gabriel-agbese/" target="_blank">Linkedin</a></p>
-                            <SlSocialLinkedin className="icon"/>
+                <div className="about section abouttext">
+                    <div className="side">
+                        <div className="">
+                            <p className="bigtext header">About</p>
+                            <div className="classname"></div>
                         </div>
-                        <div className="about-button blur midtext">
-                            <p className="smidtext"><a className="default-link" href="./CV-December-2023(2).pdf" target="_blank">Resume</a></p>
-                            <CgFileDocument className="icon"/>
+                        <div className="linksbio">
+                            <div className="links midtext"><img src={memoji} alt="BigCo Inc. logo" className="memoji" /></div>
+                            <div className="biotext smalltext">
+                                <div className="about-button blur midtext">
+                                    <p className="smidtext"><a className="default-link" href="https://www.linkedin.com/in/gabriel-agbese/" target="_blank">Linkedin</a></p>
+                                    <SlSocialLinkedin className="icon" />
+                                </div>
+                                <div className="about-button blur midtext">
+                                    <p className="smidtext"><a className="default-link" href="./CV-December-2023(2).pdf" target="_blank">Resume</a></p>
+                                    <CgFileDocument className="icon" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="firstskills blur smalltext">
+                            <p className="list-header">Languages:</p>
+                            <ul className="skill-list">
+                                <li>Java</li>
+                                <li>Python</li>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>JavaScript</li>
+                            </ul>
+                            <p className="list-header">Tools and Frameworks:</p>
+                            <ul className="skill-list">
+                                <li>Git</li>
+                                <li>Eclipse</li>
+                                <li>Visual Studio</li>
+                                <li>VS Code</li>
+                                <li>React</li>
+                            </ul>
+                            <ul className="skill-list">
+                                <li>Next JS</li>
+                                <li>Gatsby</li>
+                                <li>SCSS</li>
+                                <li>Tailwind</li>
+                            </ul>
+                            <p className="list-header">Operating Systems:</p>
+                            <ul className="skill-list">
+                                <li>Windows</li>
+                                <li>Android</li>
+                                <li>Linux</li>
+                            </ul>
                         </div>
                     </div>
-                </div>
-                <div className="firstskills blur smalltext">java, and co</div>
-                </div>
-                <div className="swaptab blur">
-                    <div className="biotab">
-                        <input type="radio" id="biotabradio" checked></input>
-                        <label for="biotabradio">Bio</label>
+                    <div className="swaptab blur">
+                        <div className="biotab">
+                            <input type="radio" id="biotabradio" checked></input>
+                            <label for="biotabradio">Bio</label>
+                        </div>
+                        <div className="skillstab">
+                            <input type="radio" id="skillstabradio"></input>
+                            <label for="skillstabradio">Skills</label>
+                        </div>
                     </div>
-                    <div className="skillstab">
-                        <input type="radio" id="skillstabradio"></input>
-                        <label for="skillstabradio">Skills</label>
+                    <div className="bio blur">
+                        <div className="someData" id="first">
+
+                        </div>
+                        <div className="someData" id="second"><p className="list-header">Languages:</p>
+                            <ul className="skill-list">
+                                <li>Java,</li>
+                                <li>Python,</li>
+                                <li>HTML,</li>
+                                <li>CSS,</li>
+                                <li>JavaScript</li>
+                            </ul>
+                            <p className="list-header">Tools and Frameworks:</p>
+                            <ul className="skill-list">
+                                <li>Git,</li>
+                                <li>Eclipse,</li>
+                                <li>Visual Studio,</li>
+                                <li>VS Code,</li>
+                                <li>React,</li>
+                            </ul>
+                            <ul className="skill-list">
+                                <li>Next JS,</li>
+                                <li>Gatsby,</li>
+                                <li>SCSS,</li>
+                                <li>Tailwind</li>
+                            </ul>
+                            <p className="list-header">Operating Systems:</p>
+                            <ul className="skill-list">
+                                <li>Windows,</li>
+                                <li>Android,</li>
+                                <li>Linux</li>
+                            </ul></div>
                     </div>
-                </div>
-                <div className="bio blur">
-                    <div className="someData" id="first">biotext</div>
-                    <div className="someData" id="second">skilltext</div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
